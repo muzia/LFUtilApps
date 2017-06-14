3c42d09de22eb933ebc6153f2c220470a0da20cc
lifei <lifei@glzc.com>
2017-06-14 15:19:58 +0800

    transition测试

--- app/src/main/java/com/example/admin/lfutilapp/MainActivity.java
+++ app/src/main/java/com/example/admin/lfutilapp/MainActivity.java
@@ -1,16 +1,46 @@
 package com.example.admin.lfutilapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.Fade;
+import android.transition.TransitionManager;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private ViewGroup mRootView;
+    private View tv1,tv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("MainActivity","bbbbbbbbbbbb");
-        Log.e("MainActivity","cccccccccccccc");
+//        ActivityOptions.makeSceneTransitionAnimation(activity, pairs).toBundle();
+        mRootView = (ViewGroup) findViewById(R.id.root_view);
+        mRootView.setOnClickListener(this);
+        tv1 = findViewById(R.id.tv1);
+        tv2 = findViewById(R.id.tv2);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public void overridePendingTransition(int enterAnim, int exitAnim) {
+        super.overridePendingTransition(enterAnim, exitAnim);
+    }
+    @Override
+    public void onClick(View v) {
+        //自动的根据状态的区别去生成动画效果：界面渐变
+        TransitionManager.beginDelayedTransition(mRootView,new Fade());
+        toggleVisibility(tv1,tv2);
+    }
+    private static void toggleVisibility(View... views) {
+        for (View view : views) {
+            boolean isVisible = view.getVisibility() == View.VISIBLE;
+            view.setVisibility(isVisible ? View.INVISIBLE : View.VISIBLE);
+        }
     }
 }
